bd4df294e2070118de47de0e5a677e939e3e0827
hi3rdt <tuduchiep123@gmail.com>
2025-10-05 16:09:47 +0700

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Agricultural Land',
+  description: 'A dashboard for agricultural land data.',
   generator: 'v0.app',
 }
 
